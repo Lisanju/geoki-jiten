5024c36c7e07f8582f64ac7785f5d038750b7835
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-10-29 15:04:51 -0300

    Create conversion.lua

---
+++ base/conversion.lua
@@ -0,0 +1,13 @@
+local export = {}
+
+local OK_CONS = {
+    "b", "c", "d", "g",
+    "?", "h", "k", "l",
+    "m", "n", "p", "q",
+    "r", "s", "t", "w",
+    "z", "]", "'",
+}
+
+local OK_VOW = {
+    "a", "i", "u", "e", "o",
+}
