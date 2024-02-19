5e54f0b55133d24cd4e0babeb6bb0358670af3af
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-19 20:45:18 -0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,15 @@
 import openpyxl
 
-ptbrData = openpyxl.load_workbook("data/ptbr_okinawago_data.xlsx")
-ptbrDataWS = ptbrData.active
+# Path
+wbPB = openpyxl.load_workbook("data/ptbr_okinawago_data.xlsx")
+# Worksheet ativo
+wsPB = wbPB.active
 
-print(f"Número total de linhas: {str(ptbrDataWS.max_row)}")
+# Função para encontrar palavra no worksheet
+def acharPalavra(string):
+    for i in range(1,wsPB.max_row+1):
+        for j in range(1,2):
+            if string == wsPB.cell(i,j).value:
+                print(f"Palavra encontrada: {wsPB.cell(i,j).value}")
+
+acharPalavra("?aci")
