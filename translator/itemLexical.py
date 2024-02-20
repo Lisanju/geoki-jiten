9b8bedf1bee284d3aaebd3b3952915b8f4c3a3a8
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-19 22:13:29 -0300

    Update itemLexical.py

--- translator/itemLexical.py
+++ translator/itemLexical.py
@@ -2,25 +2,28 @@ import openpyxl
 import translators as ts
 import time
 
-# Path do workbook português brasileiro (ainda não traduzido do JP para PT-BR)
-wb = openpyxl.load_workbook("../data/ptbr_okinawago_data.xlsx")
-
-# Worksheet português brasileiro ativo
+wb = openpyxl.load_workbook("data/ptbr_okinawago_data.xlsx")
 ws = wb.active
 
-# Função com loop de tradução que itera sobre cada célula da planilha
-# Caso a célula esteja vazia ou não tenha uma string como valor, não há tradução
-# Há um intervalo de 1 segundo para cada tradução
-def tradutor():
-  for i in range(1, ws.max_row + 1):
-      for j in range(1, ws.max_column + 1):
-          celulaValor = ws.cell(i, j).value
-          if celulaValor and isinstance(celulaValor, str):
-              palavraTraduzida = ts.translate_text(celulaValor, "alibaba")
-              ws.cell(row = i, column = j, value = palavraTraduzida)
-              time.sleep(1)
+linhas_processadas = 0
 
-tradutor()
+for i in range(1, ws.max_row + 1):
+    for j in range(1, ws.max_column + 1):
+        celulaValor = ws.cell(i, j).value
+        if celulaValor and isinstance(celulaValor, str):
+            try:
+                palavraTraduzida = ts.translate_text(celulaValor, "alibaba")
+                if palavraTraduzida is not None:
+                    ws.cell(row=i, column=j, value=palavraTraduzida)
+                    print(f"Linha {i} / {ws.max_row}")
+                else:
+                    print(f"A tradução para a célula ({i}, {j}) retornou None.")
+            except Exception as e:
+                print(f"Ocorreu um erro ao traduzir a célula ({i}, {j}): {e}")
+            time.sleep(1)
+    linhasProcessadas = linhasProcessadas + 1
+    if linhasProcessadas % 100 == 0:
+        wb.save("ptbr_okinawago_data.xlsx")
+        print("Arquivo salvo.")
 
-# Salvar o workbook traduzido
 wb.save("ptbr_okinawago_data.xlsx")
