f0184848b821ab4e0de3a90d3d20c9daf83c7509
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-19 21:02:58 -0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,15 +1,26 @@
 import openpyxl
 
-# Path
+# Path do workbook okinawano
+wbOK = openpyxl.load_workbook("data/ptbr_okinawago_data.xlsx")
+# Worksheet okinawano ativo
+wsOK = wbOK.active
+
+# Path do workbook português brasileiro
 wbPB = openpyxl.load_workbook("data/ptbr_okinawago_data.xlsx")
-# Worksheet ativo
+#Worksheet português brasileiro ativo
 wsPB = wbPB.active
 
-# Função para encontrar palavra no worksheet
+# Função para pesquisar palavra no worksheet, retornando sua célula e informações lexicais
 def acharPalavra(string):
-    for i in range(1,wsPB.max_row+1):
+    for i in range(1,wsOK.max_row+1):
         for j in range(1,2):
-            if string == wsPB.cell(i,j).value:
-                print(f"Palavra encontrada: {wsPB.cell(i,j).value}")
-
+            if string == wsOK.cell(i,j).value:
+                celula = wsOK.cell(i,j).value
+                palavra = wsPB.cell(i,j).value # FIX: Arrumar palavra
+                entonacao = wsOK.cell(i,j+1).value
+                classe = wsOK.cell(i,j+2).value
+                glosa = wsPB.cell(i,j+2).value # FIX: Arrumar glosa
+                print(f"Palavra encontrada: {wsOK.cell(i,j).value}\nEntonação: {entonacao}\nClasse: {classe}")
+                return celula, palavra, entonacao, classe, glosa
+                
 acharPalavra("?aci")
